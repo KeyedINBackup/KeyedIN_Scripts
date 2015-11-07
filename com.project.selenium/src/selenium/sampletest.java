427fd2a3c6584743d58c51a3dea5fedd1247aa93
Thirugnanamoorthy B <Thirugnanamoorthy B>
2015-11-07 12:16:04 +0530

    07/11/15 12:15 pm

--- com.project.selenium/src/selenium/sampletest.java
+++ com.project.selenium/src/selenium/sampletest.java
@@ -16,7 +16,7 @@ public static void main(String[] args) throws InterruptedException{
 	dr.get("https://www.google.co.in");
 	dr.findElement(By.name("q")).sendKeys("Quality");
 	dr.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-	List<WebElement> sel = dr.findElements(By.xpath("html/body/div/div[3]/form/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/div/ul"));
+	List<WebElement> sel = dr.findElements(By.className("sbsb_b"));
 	int Size = sel.size();
 	for(WebElement print:sel){
 		System.out.println(print.getText());
@@ -24,7 +24,7 @@ public static void main(String[] args) throws InterruptedException{
 	String Str1="html/body/div[1]/div[3]/form/div[2]/div[2]/div[1]/div[2]/div[2]/div[1]/div/ul/li[";
 	String Str2="]/div/div[2]";
 	Random rand = new Random(System.currentTimeMillis());
-	int rval = rand.nextInt(Size)+4;
+	int rval = rand.nextInt(Size)+3;
 	dr.findElement(By.xpath(Str1+rval+Str2)).click();
 	System.out.print("Random selection is done succesfully");
 	Thread.sleep(1000);
